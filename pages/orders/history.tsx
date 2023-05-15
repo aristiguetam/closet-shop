1c5eddcfdba0335a14b36483be16a8851e8a5ee0
aristiguetam <aristiguetam97@gmail.com>
2023-05-15 08:41:43 -0500

    Layout register and login

--- pages/orders/history.tsx
+++ pages/orders/history.tsx
@@ -1,8 +1,10 @@
 
+import NextLink from 'next/link';
+
 import { DataGrid, GridColDef, GridRenderCellParams, GridValueGetterParams } from '@mui/x-data-grid';
 
 import { ShopLayout } from '@/components/layouts'
-import { Grid, Typography, Chip } from '@mui/material';
+import { Grid, Typography, Chip, Button, Link } from '@mui/material';
 
 const columns: GridColDef[] = [
     { field: "id", headerName: 'ID', width: 100 },
@@ -13,24 +15,37 @@ const columns: GridColDef[] = [
         headerName: 'Pagada',
         description: 'Muestra información si esta pagada la orden o no',
         width: 200,
-        renderCell: (params: GridRenderCellParams ) => {
+        renderCell: (params: GridRenderCellParams) => {
             return (
-                params.row.paid 
-                ? <Chip color='success' label='Pagada' variant='outlined'/>
-                : <Chip color='error' label='No Pagada' variant='outlined'/>
+                params.row.paid
+                    ? <Chip color='success' label='Pagada' variant='outlined' />
+                    : <Chip color='error' label='No Pagada' variant='outlined' />
+            )
+        }
+    },
+
+    {
+        field: 'redirect', headerName: 'Ver orden', sortable: false, width: 200, renderCell: (params: GridRenderCellParams) => {
+            return (
+                <NextLink href={`/orders/${params.row.id}`} passHref legacyBehavior>
+                    <Link underline='always'>
+                        Orders
+                    </Link>
+                </NextLink>
             )
         }
     }
+
 ]
 
 const row = [
-    { id: 1, paid: false, fullname: 'Pedro Aristigueta' },
-    { id: 2, paid: true, fullname: 'Deiralyn Gonzalez' },
-    { id: 3, paid: false, fullname: 'Alexander Gomez' },
-    { id: 4, paid: false, fullname: 'Eduardo Lara' },
-    { id: 5, paid: true, fullname: 'Romulo Herrera' },
-    { id: 6, paid: false, fullname: 'Luciano Moran' },
-    { id: 7, paid: true, fullname: 'Victor Drija' },
+    { id: 1, paid: false, fullname: 'Pedro Aristigueta', },
+    { id: 2, paid: true, fullname: 'Deiralyn Gonzalez', },
+    { id: 3, paid: false, fullname: 'Alexander Gomez', },
+    { id: 4, paid: false, fullname: 'Eduardo Lara', },
+    { id: 5, paid: true, fullname: 'Romulo Herrera', },
+    { id: 6, paid: false, fullname: 'Luciano Moran', },
+    { id: 7, paid: true, fullname: 'Victor Drija', },
 ]
 
 const HistoryPage = () => {
