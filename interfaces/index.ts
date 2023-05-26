e2a6907d3dc0b3707d9d57876483e4443c3eb293
aristiguetam <aristiguetam97@gmail.com>
2023-05-26 16:20:49 -0500

    Eliminando del index nextAuth interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -2,7 +2,6 @@ export * as IPaypal from './paypal';
 export * from './cart';
 export * from './dashboard';
 export * from './login';
-export * from './nextAuth';
 export * from './order';
 export * from './products';
 export * from './user';
