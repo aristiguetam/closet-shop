5ea22f2108eca0e301a03e07526cbbad43e7b938
aristiguetam <aristiguetam97@gmail.com>
2023-05-27 16:47:51 -0500

    actualizacion de middleware 0.4

--- middleware.ts
+++ middleware.ts
@@ -27,6 +27,12 @@ export async function middleware(req: NextRequest) {
         return NextResponse.redirect(url);
     }
 
+    if(session){
+        if (requestedPage.includes('/auth/login' || '/auth/register')) {
+            return NextResponse.redirect(new URL('/', req.url));
+        }
+    }
+
     if (requestedPage.includes('/api/admin') && !validRoles.includes(session.user.role)) {
 
         return new Response(JSON.stringify({ message: 'No autorizado' }), {
